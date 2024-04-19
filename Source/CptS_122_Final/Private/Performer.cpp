7282674b60dc0849a16794bcb7e3f690e4948ffa
HappyGamer3194 <aryanputhran@gmail.com>
2024-04-18 17:20:16 -0700

    Added Player Movement and Empty Map

---
+++ Source/CptS_122_Final/Private/Performer.cpp
@@ -0,0 +1,60 @@
+#include "Performer.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+APerformer::APerformer()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Player Camera"));
+	Camera->SetupAttachment(RootComponent);
+	Camera->bUsePawnControlRotation = true;
+}
+
+// Called when the game starts or when spawned
+void APerformer::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void APerformer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void APerformer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAxis("MoveForward", this, &APerformer::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APerformer::MoveRight);
+
+	PlayerInputComponent->BindAxis("MouseHorizontal", this, &APerformer::MouseHorizontal);
+	PlayerInputComponent->BindAxis("MouseVertical", this, &APerformer::MouseVertical);
+}
+
+void APerformer::MoveForward(float InputValue)
+{
+	FVector ForwardDirection = GetActorForwardVector();
+	AddMovementInput(ForwardDirection, InputValue);
+}
+
+void APerformer::MoveRight(float InputValue)
+{
+	FVector RightDirection = GetActorRightVector();
+	AddMovementInput(RightDirection, InputValue);
+}
+
+void APerformer::MouseHorizontal(float InputValue)
+{
+	AddControllerYawInput(InputValue);
+}
+
+void APerformer::MouseVertical(float InputValue)
+{
+	AddControllerPitchInput(InputValue);
+}
---
+++ Source/CptS_122_Final/Public/Performer.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Performer.generated.h"
+
+UCLASS()
+class CPTS_122_FINAL_API APerformer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APerformer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(EditAnywhere)
+	class UCameraComponent* Camera;
+	void MoveForward(float InputValue);
+	void MoveRight(float InputValue);
+
+	void MouseHorizontal(float InputValue);
+	void MouseVertical(float InputValue);
+};
